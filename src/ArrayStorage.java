bd7a27603429497ebc0d05f98f3ffbf0c60e83f4
67AlphaBravo67 <lovecars527@gmail.com>
2021-12-31 22:39:36 +0300

    update

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,47 @@
+import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        Arrays.fill(storage,0,size(), null);
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if(storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                int numMoved = size - i - 1;
+                if (numMoved > 0) {
+                    System.arraycopy(storage, i + 1, storage, i, numMoved);
+                }
+                size--;
+            }
+        }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
+
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
